7d650f0c1cb647f0157722142eb7192301f1b206
Tianyi Zheng <92.tianyi@gmail.com>
2020-11-18 11:06:24 -0800

    Update

---
+++ R/scatterwithoutlier.R
@@ -0,0 +1,82 @@
+#' Create scatterplot for two quantitative variables with labeled outliers
+#'
+#' @param data a list (dataframe, tibble, etc…) with two quantitative variables.
+#' @param xy ogicals. If TRUE then the first column of the list would assumed to be the independent variable, and vice versa for FALSE. The default if TRUE.
+#' @param cooksd a non-negative numerical value for cook’s distance threshold. The default is 0.5.
+#' @param outputcount logicals. If TRUE then print a message on the count of influential observations.
+#' @param verbose logicals. If TRUE then print a message to inform the user that the process is running.
+#' @param ... additional arguments to be passed to the low level regression fitting functions.
+#'
+#' @return A ggplot object(scatterplot) with fitted regression line and red labelled influential points.
+#' @export
+#'
+#' @examples
+#'
+#' scatterwithoutlier(gapminder %>%
+#' select(lifeExp,pop))
+#'
+#' scatterwithoutlier(gapminder %>%
+#' select(pop,lifeExp),xy=FALSE,cooksd=0.05,verbose = TRUE,outputcount = TRUE)
+#'
+#' scatterwithoutlier(gapminder %>%
+#' select(lifeExp,gdpPercap),cooksd = 0.05)
+
+scatterwithoutlier<-function(data, xy=TRUE, cooksd=0.5, outputcount=FALSE, verbose=FALSE,...){
+  library(tidyverse)
+  if(typeof(data)!="list"){
+    stop('The function expect input data as list\n',
+         'but the input is: ', typeof(data)[1])
+  }
+  if(ncol(data)!=2){
+    stop('The function expect two columns\n',
+         'but the input has: ', ncol(data))
+  }
+  if(!is.numeric(data[[1]])|!is.numeric(data[[2]])){
+    stop('The function expect quantinative/numberic variables.')
+  }
+  if(!is.numeric(cooksd)|cooksd<0){
+    stop('cooksd needs to be a non-negative number')
+  }
+  if(!is.logical(xy)){
+    stop('xy needs to be a boolean value')
+  }
+  if(!is.logical(outputcount)){
+    stop('outputcount needs to be a boolean value')
+  }
+  if(!is.logical(verbose)){
+    stop('verbose needs to be a boolean value')
+  }
+  if(xy==TRUE){
+    x=data[[1]]
+    y=data[[2]]
+    xname=colnames(data)[1]
+    yname=colnames(data)[2]
+
+  }
+  if(xy==FALSE){
+    x=data[[2]]
+    y=data[[1]]
+    xname=colnames(data)[2]
+    yname=colnames(data)[1]
+
+  }
+  model<-lm(y~x,...)
+  if(verbose==TRUE){
+    cat("Linear model fitting done, now creating plot...")
+  }
+
+  if(outputcount==TRUE){
+    Infobvec<-as.vector(cooks.distance(model)>cooksd)
+    Infobnum<-length(subset(Infobvec,Infobvec==TRUE))
+    cat("We have",Infobnum,"influential observations","at",cooksd,"cook's distance threshold")
+  }
+  data %>%
+    mutate(Infob=cooks.distance(model)>cooksd) %>%
+    ggplot(aes(x,y))+
+    geom_point(aes(color=Infob))+
+    geom_smooth(method=lm, se=FALSE)+
+    scale_color_manual(values=c("Black", "Red"))+
+    theme_bw()+
+    labs(x=xname,y=yname)+
+    theme(legend.position = "none")
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(scatterwithoutlier)
+
+test_check("scatterwithoutlier")
